4c46b35504408dc6e9a25f6e0d00fa222280db34
ShobujDas <shobujd6.gmail.com>
2024-04-30 12:59:49 +0600

    new commit

---
+++ Think_In_A_Javascript_Way/01Scope.js
@@ -0,0 +1,13 @@
+// var x = 23;
+
+// parent er dunia
+function myFunc() {
+  // myFunc/child er dunia
+  var x = 10;
+  var y = 30
+  console.log(`${x} from myFunc()`);
+}
+myFunc()
+
+console.log(x) 
+// console.log(y)
